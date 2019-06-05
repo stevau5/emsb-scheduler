3ea9f30bb5b2e29daff771561711cbbdcb965c6c
Steven <stevenanthonyzanga@gmail.com>
2019-06-04 21:44:22 -0400

    added string to date functions

--- server.js
+++ server.js
@@ -1,14 +1,39 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
 
-app.get("/", (req, res) => res.send("API is Running"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/test", (req, res) => {
+// app.get("/", (req, res) => res.send("API is Running"));
+
+// SAB ROUTE
+app.get("/SAB", (req, res) => {
   res.sendFile(__dirname + "/Views/index.html");
 });
 
+app.post("/generateSAB", (req, res) => {
+  res.send("Shit works man");
+
+  const teacherName = req.body.tn;
+  const startDate = req.body.sd;
+  const endDate = req.body.ed;
+
+  const sd = new Date(startDate);
+  sd.setDate(sd.getDate() + 1);
+  const ed = new Date(endDate);
+  ed.setDate(ed.getDate() + 1);
+
+  const dates = [];
+
+  for (; sd.getTime() < ed.getTime() + 1; sd.setDate(sd.getDate() + 7)) {
+    dates.push(sd.toDateString());
+  }
+  console.log(dates);
+});
+
+// Prof Sales Route
 app.get("/SAB", (req, res) => res.send("SAB"));
 
+// Server Config
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("Server started on Port: " + PORT));
